5ebf033e398fb2636d7870adbdee860d9e2af256
wangeshiizzai <keshi.yvonne84@gmail.com>
2025-11-20 08:59:39 +0300

    passed some failed some

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,41 @@
-#!/usr/bin/env python3
-
 class Shoe:
-    pass
+    def __init__(self, brand, size, color="White"):
+        self.brand = brand
+        self.size = size
+        self.color = color
+
+    @property
+    def brand(self):
+        return self._brand
+
+    @brand.setter
+    def brand(self, value):
+        if isinstance(value, str) and value.strip():
+            self._brand = value
+        else:
+            raise ValueError("Brand must be a non-empty string.")
+
+    @property
+    def size(self):
+        return self._size
+
+    @size.setter
+    def size(self, value):
+        if isinstance(value, int) and value > 0:
+            self._size = value
+        else:
+            print("size must be an integer")
+
+    @property
+    def color(self):
+        return self._color
+
+    @color.setter
+    def color(self, value):
+        if isinstance(value, str) and value.strip():
+            self._color = value
+        else:
+            raise ValueError("Color must be a non-empty string.")
+
+    def cobble(self):
+        print("Your shoe has been repaired.")
